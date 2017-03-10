ceb2a84c2ad1445deefcb327b43a45cfc84b3b1d
赵瑞鹏 <rpdict@gmail.com>
2017-03-10 19:21:07 +0800

    Add socket.io

---
+++ server.js
@@ -0,0 +1,78 @@
+#!/usr/bin/env node
+
+var WebSocketServer = require('websocket').server;
+var WebSocketClient = require('websocket').client;
+var WebSocketFrame = require('websocket').frame;
+var WebSocketRouter = require('websocket').router;
+var W3CWebSocket = require('websocket').w3cwebsocket;
+var http = require('http');
+var clients = [];
+
+var server = http.createServer(function(request, response) {
+    console.log((new Date()) + ' Received request for ' + request.url);
+    response.writeHead(404);
+    response.end();
+});
+server.listen(8080, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server,
+    // You should not use autoAcceptConnections for production
+    // applications, as it defeats all standard cross-origin protection
+    // facilities built into the protocol and the browser.  You should
+    // *always* verify the connection's origin and decide whether or not
+    // to accept it.
+    autoAcceptConnections: false
+});
+
+function originIsAllowed(origin) {
+    // put logic here to detect whether the specified origin is allowed.
+    return true;
+}
+
+wsServer.on('request', function(request) {
+    if (!originIsAllowed(request.origin)) {
+        // Make sure we only accept requests from an allowed origin
+        request.reject();
+        console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+        return;
+    }
+
+    var connection = request.accept('echo-protocol', request.origin);
+    clients.push(connection);
+    console.log((new Date()) + ' Connection accepted.');
+    // console.log(connection.send);
+
+
+    //  for ( a in request.httpRequest.client) {
+    //    console.log(a);
+    //  }
+    // console.log(request.httpRequest);
+
+    connection.on('message', function(message) {
+
+        // if (message.type === 'utf8') {
+        //     console.log('Received Message: ' + message.utf8Data);
+        //     connection.sendUTF(message.utf8Data);
+        // }
+        // else if (message.type === 'binary') {
+        //     console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
+        //     connection.sendBytes(message.binaryData);
+        // }
+        console.log('Received Message: ' + message.utf8Data);
+        var json = JSON.stringify({
+            type: 'message',
+            data: message.utf8Data
+        });
+        console.log(json);
+        console.log(clients.length);
+        for (i = 0; i < clients.length; i++) {
+            clients[i].sendUTF(json);
+        }
+    });
+    connection.on('close', function(reasonCode, description) {
+        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
+    });
+});
